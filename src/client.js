7532788989ccb3d161e1e5d3b152f627ea009303
krishna agrawal <agrawalkrishna522@gmail.com>
2024-01-20 21:44:45 +0530

    adding sanity

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId: "uc4v3f6c",
+    dataset: "production"
+})
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'uc4v3f6c',
+    dataset: 'production'
+  }
+})
